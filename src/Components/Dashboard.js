306e3990dcfc285b983f1c914fc5b22b10136ed4
Travis Kamerer <tkamerer@pdx.edu>
2021-11-23 14:31:51 -0800

    Fixing issues with dashboard, caused npm start not to work, error:axios not found

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,5 +1,5 @@
-import React from 'react'
-//import axios from 'axios';
+/*import React from 'react'
+import axios from 'axios'
 
 
 class Dashboard extends React.Component {
@@ -46,3 +46,4 @@ class Dashboard extends React.Component {
 }
 
 export default Dashboard;
+*/
