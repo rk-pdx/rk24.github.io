d898d158ca6445f1c8b5549be8233a54a1ac7240
Travis Kamerer <tkamerer@pdx.edu>
2021-11-23 12:00:22 -0800

    Fixed so npm start can run, dashboard was making an issue, is fully commented out

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,8 +1,8 @@
-import React from 'react'
+/*import React from 'react'
 import request from 'request';//To be modified.
 
 
-/*class Dashboard extends React.Component {
+class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
