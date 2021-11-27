425fb319b5e1c32f7a11d5e079654f540046ffcd
Rajat Kulkarni <rk24@pdx.edu>
2021-11-27 11:10:43 -0800

    REQUIRED: react-router-dom. Added more functionality to Topbar: it will now programmatically read in what the user wants to search and handle state accordingly.

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -1,14 +1,42 @@
 import React, { useState } from 'react'
+import { Router, Navigate, Route } from 'react-router-dom';
+import Bitcoin from './bitcoin'
 import "./topbar.css"
 
 class Topbar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            userInput: ""
+            userInput: "",
+            items: [],
+            linkTo: ""
         }
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        var axios = require("axios").default;
+
+        var options = {
+            method: 'GET',
+            url: 'https://coinranking1.p.rapidapi.com/coins',
+            headers: {
+                'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+                'x-rapidapi-key': '744989c25amsh1bcd9639b8a662cp14a242jsneff4a277f4ea'
+            }
+        };
+
+        axios.request(options).then((response) => {
+            //console.log(response.data.data.coins);
+
+            this.setState({
+                items: response.data.data.coins
+            })
+        }).catch(function (error) {
+            console.log(error);
+        });
     }
 
     handleChange(event) {
@@ -16,13 +44,44 @@ class Topbar extends React.Component {
         console.log(this.state.userInput);
     }
 
+    //Check if the user input matches any currency in call
+    handleSubmit(event) {
+        console.log("Submitted")
+        event.preventDefault();
+
+        for (let i = 0; i < this.state.items.length; i++) {
+            if (this.state.userInput == this.state.items[i].name) {
+                console.log("match")
+                let tempStr = `/${this.state.userInput}`
+                this.setState({
+                    linkTo: tempStr 
+                })
+            } 
+        }
+    }
+
     render() {
+        var { linkTo } = this.state;
+
+        /*if (linkTo != "") {
+            return (
+                <Router>
+                    
+                        <Route path='/' component={Bitcoin} />
+                   
+                </Router>
+            );
+        }*/
+
         return (
             <div>
                 <div className="topbar">
                     <div className="topbarContainer">
                         <div className="title"> My Dashboard</div>
+                        <form onSubmit={this.handleSubmit}>
                             <input className= "searchBar" type="text" placeholder="Search" onChange={this.handleChange}></input>
+                            <button type="submit">Search</button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -32,6 +91,7 @@ class Topbar extends React.Component {
 
 export default Topbar;
 
+//BEN's ORIGINAL CODE
 /*export default function Topbar() {
     const [value, setValue] = useState(1);
     return (
