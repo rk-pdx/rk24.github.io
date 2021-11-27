23a3b58da43be9ab1d225e6db9818e99723b86a4
Rajat Kulkarni <rk24@pdx.edu>
2021-11-26 19:57:40 -0800

    Changed Topbar.js from functional component to class component to handle searching functionality easily.

--- src/Components/Topbar/Topbar.js
+++ src/Components/Topbar/Topbar.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./topbar.css"
 
-export default function Topbar() {
+class Topbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userInput: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({userInput: event.target.value});
+        console.log(this.state.userInput);
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="topbar">
+                    <div className="topbarContainer">
+                        <div className="title"> My Dashboard</div>
+                            <input className= "searchBar" type="text" placeholder="Search" onChange={this.handleChange}></input>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Topbar;
+
+/*export default function Topbar() {
+    const [value, setValue] = useState(1);
     return (
         <div className="topbar">
             <div className="topbarContainer">
@@ -10,4 +42,4 @@ export default function Topbar() {
             </div>
         </div>
     )
-}
+}*/
