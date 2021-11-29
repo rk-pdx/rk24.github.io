f8089121d9d4d46d7d28323aac36c1531b4cbc41
Benjamin Lostheart <los3@pdx.edu>
2021-11-28 23:50:50 -0800

    added converter

--- src/App.js
+++ src/App.js
@@ -3,19 +3,48 @@ import React from 'react'
 import Dashboard from './Components/Dashboard/Dashboard';
 import Topbar from './Components/Topbar/Topbar';
 import "./app.css"
+import Converter from './Components/Converter/Converter';
+
 
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-           items: [],
-           filterOn: ""
+            items: [],
+            isLoaded: false,
+            filterOnApi: this.props.filterOnApi,
+            filterOn: ""
         }
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
+
+    componentDidMount() {
+        var axios = require("axios").default;
+
+        var options = {
+            method: 'GET',
+            url: 'https://coinranking1.p.rapidapi.com/coins',
+            headers: {
+                'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+                'x-rapidapi-key': '744989c25amsh1bcd9639b8a662cp14a242jsneff4a277f4ea'
+            }
+        };
+
+        axios.request(options).then((response) => {
+            console.log(response.data.data.coins);
+            console.log(this.state.filterOnApi)
+            this.setState({
+                items: response.data.data.coins,
+                isLoaded: true
+            })
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+
+        
 
     handleChange(event) {
         this.setState({filterOn: event.target.value});
@@ -29,15 +58,18 @@ class App extends React.Component {
 
     render() {
         var { filterOn } = this.state;
+        var { isLoaded, items } = this.state;
 
         return(
             <div>
                 <Topbar/>
+               
                 <h1>Dashboard</h1>
                 <form  className="SearchForm" onSubmit={this.handleSubmit}>
                     <input className= "searchBar" type="text" placeholder="Search" onChange={this.handleChange}></input>
                     <button type="submit">Search</button>
                 </form>
+                <Converter coins = {items}></Converter>
                 <Dashboard filterOn={this.state.filterOn}/>
             </div>
         );
