c0b87f47e69931b9508df82ee1bf28ae68c74922
Rajat Kulkarni <rk24@pdx.edu>
2021-11-28 22:46:21 -0800

    Completed Displaying Individual coin data upon user clicking "Details" button

--- src/Components/CoinData.js
+++ src/Components/CoinData.js
@@ -1,11 +1,43 @@
 
-import React from 'react'
+import React from 'react';
+import Coin from './Coin/Coin';
+
 
 
 function CoinData({ data }) {
     console.log(data.name)
     return (
-       <table className="Table">
+        <table className="Table">
+           <tr>
+                <th>
+                    Rank
+                </th>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Price
+                </th>
+            </tr>
+            <Coin coin = {data} rank = {data.rank} name = {data.name} price = {data.price}/>
+       </table>
+    )
+}
+
+export default CoinData
+
+//<Coin rank = {item.rank} name = {item.name} price = {item.price} description = {item.description} circulatingSupply = {item.circulatingSupply} symbol = {item.symbol} numberOfExchanges = {item.numberOfExchanges} numberOfMarkets = {item.numberOfMarkets} slug = {item.slug} uuid = {item.uuid} websiteUrl = {item.websiteUrl} />
+/*{coinData.map(item => (
+    <div key={item.id} className="Row">
+                        
+    </div>
+))}*/
+
+
+/*
+ORIGINAL RETURN CONTENTS
+
+<table className="Table">
            <tr>
                 <th>
                     Rank
@@ -47,14 +79,4 @@ function CoinData({ data }) {
             COINDATA:
            
        </table>
-    )
-}
-
-export default CoinData
-
-//<Coin rank = {item.rank} name = {item.name} price = {item.price} description = {item.description} circulatingSupply = {item.circulatingSupply} symbol = {item.symbol} numberOfExchanges = {item.numberOfExchanges} numberOfMarkets = {item.numberOfMarkets} slug = {item.slug} uuid = {item.uuid} websiteUrl = {item.websiteUrl} />
-/*{coinData.map(item => (
-    <div key={item.id} className="Row">
-                        
-    </div>
-))}*/
+*/
